3f5d31e3c2852aa456e76ca120994ffbb4af958a
OLIWIERKARDYNAL-STUD <G00434405@atu.ie>
2025-10-08 17:24:29 +0100

    Added Student info to while loop

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -12,9 +12,17 @@ public class StudentApp {
         sc.nextLine(); // consume leftover newline before nextLine() calls
         int i = 0;
         while (i < count) {
-            // TODO: prompt for name, email, course using sc.nextLine()
-            // TODO: create a Student using one of your constructors
-            // TODO: add the new Student to students
+            System.out.print("Enter student name: ");
+            String name = sc.nextLine();
+            names.add(name);
+            System.out.print("Enter student email: ");
+            String email = sc.nextLine();
+            names.add(email);
+            System.out.print("Enter student course: ");
+            String course = sc.nextLine();
+            names.add(course);
+
+
             i++;
         }
 
