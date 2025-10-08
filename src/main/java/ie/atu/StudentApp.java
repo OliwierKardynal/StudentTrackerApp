e5b21f0ef6c8f5bc6224f08570e6f363c233d57a
OLIWIERKARDYNAL-STUD <G00434405@atu.ie>
2025-10-08 16:45:55 +0100

    Simple app to manage names using arraylists

---
+++ src/main/java/ie/atu/StudentApp.java
@@ -0,0 +1,13 @@
+package ie.atu;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Jane");
+        names.add("Julie");
+    }
+}
